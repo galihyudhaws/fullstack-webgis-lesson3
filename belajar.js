9663fb0cb03d9eb4c847cf60ba647abab96f31bf
Galih Yudha <galih.yudha94@gmail.com>
2023-08-12 23:42:22 +0700

    commit 20230812_2342

--- belajar.js
+++ belajar.js
@@ -1,4 +1,27 @@
-console.log('Namaku Galih')
-console.log('Saya belajar JS')
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Halo Galih!";
+// ----------------------------------
+// Pengenalan Variabel dan Tipe Data
+// ----------------------------------
+// console.log('Saya belajar JS');
+// console.log(typeof('hello world'));
+
+// const myHeading = document.querySelector("h1");
+// myHeading.textContent = "Halo Galih!";
+
+// let myVariable = null;
+// console.log(myVariable);
+// console.log(typeof(myVariable));
+
+// let payung;
+// console.log(payung);
+// console.log(typeof(payung));
+
+// let indonesia = false;
+// console.log(indonesia);
+
+// let angka = 1 <= 2;
+// console.log(angka);
+// console.log(typeof(indonesia), typeof(angka));
+
+// let angka1 = 5;
+// let angka2 = 8;
+// console.log(angka1,angka2);
