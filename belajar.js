51eb41b3509aef6eb3fa9d9fd66aeb6b24c08c4f
Galih Yudha <galih.yudha94@gmail.com>
2023-08-13 08:32:29 +0700

    commit 20230813_0832

--- belajar.js
+++ belajar.js
@@ -25,3 +25,57 @@
 // let angka1 = 5;
 // let angka2 = 8;
 // console.log(angka1,angka2);
+
+// let myVariable = "Bob";
+// console.log(myVariable);
+// myVariable = "Steve";
+// console.log(myVariable);
+
+//-----------------------------------
+// VARIABLE, CONST, LET, VAR
+//-----------------------------------
+// var tempat = 'Surabaya'
+// console.log(tempat);
+
+// let nama = 'Bambang'
+// console.log(nama);
+
+// nama = "Galih"
+// console.log(nama);
+
+// const phi = 3.14
+// console.log(phi);
+
+// phi = 4
+// console.log(phi);
+
+// --------------------------------
+// OPERATOR
+// --------------------------------
+console.log(5+2);
+console.log('Galih'+'Yudha');
+console.log('50'+2);
+console.log(5+'20');
+
+console.log('50' * 2);
+console.log(5 * '20');
+console.log(10/2);
+
+// NaN : Not a Number
+console.log('Galih'*'Yudha'); 
+console.log('Galih'-'a');
+console.log('Galih'/'a');
+
+//Deklarasi Variabel, strict equality
+//Disarankan gunakan === instead == untuk menghindari bug
+let laptop = 'HP';
+console.log(laptop);
+
+laptop = 'Lennovo';
+console.log(laptop);
+
+console.log(laptop == 'Lennovo');
+console.log(laptop == 'HP');
+
+console.log(laptop === 'Lennovo');
+console.log(laptop !== 'HP');
